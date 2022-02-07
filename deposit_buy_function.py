a39b112911202ae6dc03999a01ad75b0f6110fbc
adhillo <12418963+adhillo@users.noreply.github.com>
2022-02-06 21:12:50 -0500

    clearer logs

--- deposit_buy_function.py
+++ deposit_buy_function.py
@@ -22,11 +22,13 @@ deposit_on_run = True
 investment_size = 100
 
 # Set up your recurring buy goals, add new pairs to buy with USD
-# for example to buy link add the line -> "buys['LINK-USD'] = {'percent': .1}"
+# for example to buy link add the line -> "buys['LINK-USD'] = {'percent': .1, 'rounding': 3}"
 # make sure your percentages do not go above a total of 1 (i.e, 100%)
+# the ronding is set based on the smallest unit can buy on Coinbase Pro
+# check logs for failed buys for more details - you may need to adjust rounding for different coins
 buys = {}
-buys['BTC-USD'] = {'percent': .8}
-buys['ETH-USD'] = {'percent': .2}
+buys['BTC-USD'] = {'percent': .8, 'rounding': 5}
+buys['ETH-USD'] = {'percent': .2, 'rounding': 5}
 
 
 ########################################################
@@ -38,22 +40,30 @@ def deposit_and_invest(event, context):
 
     # check if we will deposit on each run
     if deposit_on_run:
-        auth_client.deposit(amount=investment_size, currency='USD', payment_method_id=fiat_funding_account_id)
+        deposit_response = auth_client.deposit(amount=investment_size, currency='USD', payment_method_id=fiat_funding_account_id)
+        print(f'Deposit Info:{deposit_response}')
+        print('\n')
 
     # iterate through dictionary of buys & 
     for key in buys.keys():
 
+        print(key)
+
         # get bid price of a product
         currentPrice = auth_client.get_product_ticker(key)['bid']
 
-        # place limit order based on bid price, reduce total spend by 1% to account for possible fees (highly conservative)
-        auth_client.buy(
+        print(currentPrice)
+
+        # place limit order based on bid price, reduce total spend by 3% to account for possible fees (highly conservative)
+        order = auth_client.buy(
                 price=currentPrice,
-                size=round(buys[key]['percent']*investment_size/float(currentPrice) * .99, 5),
+                size=round(buys[key]['percent']*investment_size/float(currentPrice) * .97, buys[key]['rounding']),
                 order_type='limit',
                 product_id= key,
                 overdraft_enabled=False
             )
+        
+        print(order)
+        print('\n')
 
     return 'deposit & buy functon ran - check logs'
-
